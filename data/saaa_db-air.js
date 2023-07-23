093a4052801e2661a68e75c966b63e47300dadef
RNSgnaolin <137301909+RNSgnaolin@users.noreply.github.com>
2023-07-22 21:52:27 -0300

    Content
    Added new data

--- data/saaa_db-air.js
+++ data/saaa_db-air.js
@@ -8,5 +8,614 @@ var aircrafts = [
         "agency": "N/A",
         "agencyType": "N/A",
         "aircraftModel": "Buckingham Maverick",
+    },
+    {
+        "tailNumber": "N442TK",
+        "callsign": "N/A",
+        "regName": "Amelia Robinson",
+        "regPH": "76641538",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "JoBuilt Velum"
+    },
+    {
+        "tailNumber": "N607BE",
+        "callsign": "N/A",
+        "regName": "Felix Unsworth",
+        "regPH": "9577465",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "JoBuilt Mammatus"
+    },
+    {
+        "tailNumber": "N738IA",
+        "callsign": "N/A",
+        "regName": "John Pickert",
+        "regPH": "27822909",
+        "aircraftType": "Commercial",
+        "agency": "IMEX Logistics",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Velum"
+    },
+    {
+        "tailNumber": "N4639IA",
+        "callsign": "N/A",
+        "regName": "John Pickert",
+        "regPH": "27822909",
+        "aircraftType": "Commercial",
+        "agency": "IMEX Logistics",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N661SB",
+        "callsign": "Skybound 661",
+        "regName": "Logan Parks",
+        "regPH": "29548254",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Nimbus"
+    },
+    {
+        "tailNumber": "N106JS",
+        "callsign": "N/A",
+        "regName": "Patricio Montoya",
+        "regPH": "11407467",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "aircraftModel": "JoBuilt Mammatus"
+    },
+    {
+        "tailNumber": "N660SB",
+        "callsign": "Skybound 660",
+        "regName": "Logan Parks",
+        "regPH": "29548254",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Mammoth Dodo"
+    },
+    {
+        "tailNumber": "SBX001",
+        "callsign": "N101SB",
+        "regName": "Senorise Boykin",
+        "regPH": "5550170",
+        "aircraftType": "Commercial",
+        "agency": "N/A",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Velum"
+    },
+    {
+        "tailNumber": "N658SB",
+        "callsign": "Skybound 658",
+        "regName": "Logan Parks",
+        "regPH": "29548254",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Nimbus"
+    },
+    {
+        "tailNumber": "N659SB",
+        "callsign": "Skybound 659",
+        "regName": "Logan Parks",
+        "regPH": "29548254",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Velum 5S"
+    },
+    {
+        "tailNumber": "N713AB",
+        "callsign": "N/A",
+        "regName": "Charlie Lee",
+        "regPH": "8875",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "JoBuilt Velum 5S"
+    },
+    {
+        "tailNumber": "N22CX",
+        "callsign": "N/A",
+        "regName": "Lauren Wilkerson",
+        "regPH": "35849",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "JoBuilt Velum 5S"
+    },
+    {
+        "tailNumber": "NHC042",
+        "callsign": "Skybound 042",
+        "regName": "Logan Parks",
+        "regPH": "29548254",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Maibatsu Frogger"
+    },
+    {
+        "tailNumber": "XMO618",
+        "callsign": "BARK01",
+        "regName": "Kirby Barker",
+        "regPH": "7580",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "Private",
+        "aircraftModel": "Nagasaki Buzzard"
+    },
+    {
+        "tailNumber": "NLS201R",
+        "callsign": "Air Nolia",
+        "regName": "Raheme Bell",
+        "regPH": "5188",
+        "aircraftType": "Private Jet",
+        "agency": "Magnolia Records Inc.",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Nimbus"
+    },
+    {
+        "tailNumber": "N001AM",
+        "callsign": "MARCO1",
+        "regName": "Armani Marco",
+        "regPH": "80215808",
+        "aircraftType": "Commercial",
+        "agency": "House of Marco",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham SuperVolito C."
+    },
+    {
+        "tailNumber": "H110TA",
+        "callsign": "HT1",
+        "regName": "Melissa Cole",
+        "regPH": "47301826",
+        "aircraftType": "Commercial",
+        "agency": "Hang Ten Adventures",
+        "agencyType": "Private",
+        "aircraftModel": "Maibatsu Frogger"
+    },
+    {
+        "tailNumber": "N444SA",
+        "callsign": "SAM1",
+        "regName": "Seon-mi Won",
+        "regPH": "05803293",
+        "aircraftType": "Commercial",
+        "agency": "Samyeong Inc.",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Velum 5S"
+    },
+    {
+        "tailNumber": "N916PD",
+        "callsign": "Air Zero",
+        "regName": "Shane Higgins",
+        "regPH": "2731",
+        "aircraftType": "Utility",
+        "agency": "Los Santos Police Department",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N43CU",
+        "callsign": "HT-55",
+        "regName": "Dexter Marshall",
+        "regPH": "97203707",
+        "aircraftType": "Commercial",
+        "agency": "Los Santos Fire Department",
+        "agencyType": "Government",
+        "aircraftModel": "Jetsam Cargobob"
+    },
+    {
+        "tailNumber": "NOLIA1",
+        "callsign": "NLA1",
+        "regName": "Melissa Cole",
+        "regPH": "47301826",
+        "aircraftType": "Commercial",
+        "agency": "Hang Ten Adventures",
+        "agencyType": "Private",
+        "aircraftModel": "Maibatsu Frogger"
+    },
+    {
+        "tailNumber": "GAR708",
+        "callsign": "ULSAIR1",
+        "regName": "Nikos Karagiorgis",
+        "regPH": "6151066",
+        "aircraftType": "Commercial",
+        "agency": "University of Los Santos, San Andreas",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N287GP",
+        "callsign": "Postal",
+        "regName": "Charlotte Trudeau",
+        "regPH": "9762294",
+        "aircraftType": "Commercial",
+        "agency": "GoPostal Air",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "N225GP",
+        "callsign": "Postal",
+        "regName": "Charlotte Trudeau",
+        "regPH": "9762294",
+        "aircraftType": "Commercial",
+        "agency": "GoPostal Air",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "N568GP",
+        "callsign": "Postal",
+        "regName": "Charlotte Trudeau",
+        "regPH": "9762294",
+        "aircraftType": "Commercial",
+        "agency": "GoPostal Air",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "N194GP",
+        "callsign": "Postal",
+        "regName": "Charlotte Trudeau",
+        "regPH": "9762294",
+        "aircraftType": "Commercial",
+        "agency": "GoPostal Air",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "N225SG",
+        "callsign": "Postal",
+        "regName": "Charlotte Trudeau",
+        "regPH": "9762294",
+        "aircraftType": "Commercial",
+        "agency": "GoPostal Air",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "N313SF",
+        "callsign": "KLSN-10",
+        "regName": "Melissa Rhodes",
+        "regPH": "69350330",
+        "aircraftType": "Commercial",
+        "agency": "Los Santos News Network",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Swift"
+    },
+    {
+        "tailNumber": "IPM108",
+        "callsign": "AE1",
+        "regName": "Emily Whitman",
+        "regPH": "1237",
+        "aircraftType": "Commercial",
+        "agency": "Aurum Energy",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Polmav"
+    },
+    {
+        "tailNumber": "N407BG",
+        "callsign": "Goodie 407",
+        "regName": "Branch Goodwater",
+        "regPH": "310310",
+        "aircraftType": "Commercial",
+        "agency": "Goodwater Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham SuperVolito"
+    },
+    {
+        "tailNumber": "LS1365",
+        "callsign": "N550SB",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "LS1230",
+        "callsign": "N420SB",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Duster"
+    },
+    {
+        "tailNumber": "KCR550",
+        "callsign": "N657SB",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Mammatus"
+    },
+    {
+        "tailNumber": "N646LS",
+        "callsign": "N646SB",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Velmum"
+    },
+    {
+        "tailNumber": "VCK550",
+        "callsign": "N550SB",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "Skybound Aviation",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "RBN074",
+        "callsign": "N33GV",
+        "regName": "Noah Wade",
+        "regPH": "58849129",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "Buckingham Swift"
+    },
+    {
+        "tailNumber": "N288CR",
+        "callsign": "N/A",
+        "regName": "Cyril Regas",
+        "regPH": "4824346",
+        "aircraftType": "Private",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "Western Company Cuban 800"
+    },
+    {
+        "tailNumber": "N-LS6325",
+        "callsign": "N000FF",
+        "regName": "José Feliz",
+        "regPH": "28733377",
+        "aircraftType": "Commercial",
+        "agency": "Feliz Fish",
+        "agencyType": "Private",
+        "aircraftModel": "Mammoth Dodo"
+    },
+    {
+        "tailNumber": "N401KR",
+        "callsign": "TA1301W",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "McKenzie Aviation Services",
+        "agencyType": "Private",
+        "aircraftModel": "Maibatsu Frogger"
+    },
+    {
+        "tailNumber": "LS1230",
+        "callsign": "TA101A",
+        "regName": "Timmy McCoy",
+        "regPH": "49295407",
+        "aircraftType": "Commercial",
+        "agency": "McKenzie Aviation Services",
+        "agencyType": "Private",
+        "aircraftModel": "Western Company Duster"
+    },
+    {
+        "tailNumber": "N617SL",
+        "callsign": "SAR-002",
+        "regName": "Jezebel Tran",
+        "regPH": "7247",
+        "aircraftType": "Commercial",
+        "agency": "San Airdreas",
+        "agencyType": "Private",
+        "aircraftModel": "Buckingham Swift"
+    },
+    {
+        "tailNumber": "N44432",
+        "callsign": "N/A",
+        "regName": "Zoe Seeker",
+        "regPH": "08787243",
+        "aircraftType": "Commercial",
+        "agency": "Blaine Aviation",
+        "agencyType": "Government",
+        "aircraftModel": "JoBuilt Velum 5S"
+    },
+    {
+        "tailNumber": "N-420BI",
+        "callsign": "SAR001",
+        "regName": "Jezebel Tran",
+        "regPH": "7247",
+        "aircraftType": "Commercial",
+        "agency": "San Airdreas",
+        "agencyType": "Private",
+        "aircraftModel": "Maibatsu Frogger"
+    },
+    {
+        "tailNumber": "N201OD",
+        "callsign": "OD 11",
+        "regName": "Jimmy O'Dwyer",
+        "regPH": "5555498",
+        "aircraftType": "Commercial",
+        "agency": "O'Dwyer Tours",
+        "agencyType": "Private",
+        "aircraftModel": "JoBuilt Velum 5S"
+    },
+    {
+        "tailNumber": "N734RR",
+        "callsign": "N/A",
+        "regName": "Ayala Hart",
+        "regPH": "14364605",
+        "aircraftType": "Commercial",
+        "agency": "N/A",
+        "agencyType": "N/A",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N102PR",
+        "callsign": "N/A",
+        "regName": "Hannah Hartford",
+        "regPH": "1533",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Park Rangers",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N101BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "JoBuilt Mammatus"
+    },
+    {
+        "tailNumber": "N102BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "JoBuilt Mammatus"
+    },
+    {
+        "tailNumber": "N103BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "JoBuilt Mammatus"
+    },
+    {
+        "tailNumber": "N201BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Nagasaki Buzzard"
+    },
+    {
+        "tailNumber": "N202BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Nagasaki Buzzard"
+    },
+    {
+        "tailNumber": "N203BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Nagasaki Buzzard"
+    },
+    {
+        "tailNumber": "N301BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N302BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Commercial",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Valkyrie"
+    },
+    {
+        "tailNumber": "N406AA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "HVY Skylift"
+    },
+    {
+        "tailNumber": "N380AA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Maverick"
+    },
+    {
+        "tailNumber": "N363AA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Nimbus"
+    },
+    {
+        "tailNumber": "N402BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Mammoth Dodo"
+    },
+    {
+        "tailNumber": "R-LS210",
+        "callsign": "N701BA",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Buckingham Vestra"
+    },
+    {
+        "tailNumber": "N501BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Western Company Mallard"
+    },
+    {
+        "tailNumber": "N502BA",
+        "callsign": "N/A",
+        "regName": "N/A",
+        "regPH": "N/A",
+        "aircraftType": "Utility",
+        "agency": "San Andreas Aviation Administration",
+        "agencyType": "Government",
+        "aircraftModel": "Western Company Mallard"
     }
 ];
