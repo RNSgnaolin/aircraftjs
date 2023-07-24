267575c3db66c741e80bfc955dcf8ec0065fab23
RNSgnaolin <137301909+RNSgnaolin@users.noreply.github.com>
2023-07-24 20:45:56 -0300

    Exception fix
    Added if (timeElement != null) to ensure no exception occurs in document.open()

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ function onLoad() {
 function startClock() {
     let timeElement = document.getElementById("currentLSHour");
     if(!timeElement) return;
-    
+
     setTimeOnClock();
     setInterval(setTimeOnClock, 1000);
 }
 function setTimeOnClock() {
     let timeElement = document.getElementById("currentLSHour");
     let losSantosHour = new Date().toUTCString();
-    timeElement.innerHTML = losSantosHour.slice(5, 25);
+    if (timeElement != null) {
+        timeElement.innerHTML = losSantosHour.slice(5, 25);
+    }
 }
 function generateEvidence() {
     numberOfEvidence++;
